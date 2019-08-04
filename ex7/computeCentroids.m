77d31c54dc6eba31dc5acde44d0917b045506d87
uma-pal <32111467+uma-pal@users.noreply.github.com>
2019-08-04 15:14:44 -0400

    Update computeCentroids.m

--- ex7/computeCentroids.m
+++ ex7/computeCentroids.m
@@ -26,7 +26,10 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
-
+for i = 1:K
+    selected_ex = find(idx == i);
+    centroids(i,:) = mean(X(selected_ex,:),1);
+end
 
 
 
